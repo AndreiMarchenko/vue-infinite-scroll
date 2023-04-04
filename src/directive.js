007c08b12fcac6da5bf691dfb6808d8093e0c811
AndreiMarchenko <57183525+AndreiMarchenko@users.noreply.github.com>
2023-04-04 16:19:17 +0300

    Remove console.info

--- src/directive.js
+++ src/directive.js
@@ -186,7 +186,6 @@ export default {
       expression: binding.value
     };
     const args = arguments;
-    console.info('binding', el, binding, vnode)
     el[ctx].vm.$nextTick(function () {
       if (isAttached(el)) {
         doBind.call(el[ctx], args);
